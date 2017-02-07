90aa57b4e99aa404b6a01a0d1ac7777fadee0f67
puppetmaster3 <vic@vics-MacBook-Air.local>
2017-02-07 15:00:43 -0500

    Touch

--- webApp/www/_js/setup.js
+++ webApp/www/_js/setup.js
@@ -9,11 +9,18 @@ var _inAction = false // set to true when user acts; false when effect is done
 // for user actions/'control'
  const _act = new signals.Signal()
 
-/*ex use
-_act.addOnce(function(arg1, arg2) {
-	console.log(arg1, arg2)
-	return false
-})*/
+/*ex pg:
+function init() {
+	//
+}
+if(_loaded) init()
+else {
+	_act.addOnce(function(arg1, arg2) {
+		init()
+		return false
+	})
+}
+*/
 console.log('act setup')
 // <====================================================================
 
