8c1a9cf96c74afb2181e661b55f8fb46a60c7df5
puppetmaster3 <vicmasons@gmail.com>
2017-04-16 15:17:19 -0400

    d

--- srv/webroot/_js/main.js
+++ srv/webroot/_js/main.js
@@ -1,62 +1,44 @@
 'use strict'
 loadjs.ready(['dependencyIE', 'keyLibs'], {// loaded setup libs
 	success: function(){
-	loadjs([
-		'https://cdn.rawgit.com/topseed/topseed-npm/master/v1.08/deps/jquery.smoothState.js'
 
-		//images
-		,'/_js/libJs/jquery.fullpage.min.css'
-		,'/_js/libJs/jquery.fullpage.min.js'
+		loadjs.done('ready') // page ready
 
-		//data
-		,'/_js/libJs/jquery.jsForm.min.js'
+		loadjs([
+			//images
+			'/_js/libJs/jquery.fullpage.min.css'
+			,'/_js/libJs/jquery.fullpage.min.js'
 
-		], { success: function(){
-			console.log('loaded libs')
-			startApp()
-		}
+			//data
+			,'/_js/libJs/jquery.jsForm.min.js'
+
+			], { success: function(){
+				console.log('loaded libs')
+				startApp()
+			}
 	})//loadjs
 	}//suc
 })
 
 function startApp(){
 	// READY ///////////////////////////////////////////////////////////
-	A.loaded=true
-	A.act(A.LOADED)
+	loadjs.done('main')
 
-	//SS
-	let ssoptions={
-		debug: true,
-		prefetch: true,
-		cacheLength: 2,
-		repeatDelay: 450,
+	sP.ScontentID ='#content-wrapper'
+	sP.smoothPg.add(function(typ, $new, delta, $html) {
+		console.log(typ)
 
-		onStart: {
-			duration: 0, 
-			render: function (url, $container)  {
-				//console.log('-> ')
-				A.act(A.PRE) //action
-				A.inAction=true
+		if(sP.PRE==typ)  {//start
+			console.log($new)
+			//$('#content-wrapper').fadeTo(100,.2)
 
-				$('#content-wrapper').fadeTo(1000/60,.2)
+		}
+		if(sP.PAGE==typ)  {//ready
+			$(sP.ScontentID).html($new)
+			//$('#content-wrapper').fadeTo(100,1)
 
-			}//r
-		},//onS
-		onReady: {
-			duration: 0,
-			render: function ($container, $newContent) {
-				$container.html($newContent)
-				$('#content-wrapper').fadeTo(1000/30,1)
+		}
 
-				A.act(A.PAGE)// main action
-				A.inAction= false
+	})
 
-				//console.log('% <-')
-			}//ren
-		}//ready()
-	}//sso
-	
-	const smoothState= $('#ss1').smoothState(ssoptions)
-	//var endTime = new Date().getTime() - _loadStarted
-	//console.log('load time ' + endTime)
 }//startApp()
