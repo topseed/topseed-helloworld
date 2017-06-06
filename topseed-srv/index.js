3d016d284f60368015643e58e542a79f7fdf4230
Wolfgang <wgehner@gmail.com>
2017-06-06 08:39:39 -0700

    Move css to body, anchor test

--- topseed-srv/index.js
+++ topseed-srv/index.js
@@ -9,6 +9,6 @@ server.use(express.static(ServerConfig.WEBROOT))
 
 //###################### start the server
 server.listen(ServerConfig.WWW_PORT, '0.0.0.0', function() {
-	console.log('App listening on port '+ServerConfig.WWW_PORT)
+	console.log('App listening at http://localhost: '+ServerConfig.WWW_PORT)
 	console.log('Press Ctrl+C to quit.')
 })
