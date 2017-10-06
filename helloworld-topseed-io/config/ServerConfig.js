c1346b4914d19ed78794629c75ba83100dbf0f68
wgehner <wgehner@gmail.com>
2017-10-06 16:11:40 -0700

    Restructure

---
+++ helloworld-topseed-io/config/ServerConfig.js
@@ -0,0 +1,7 @@
+'use strict'
+
+class ServerConfig {
+	get WEBROOT() {return 'public'}
+	get WWW_PORT() {return 8888}
+	
+} module.exports = ServerConfig
--- topseed-srv/config/ServerConfig.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-class ServerConfig {
-	get WEBROOT() {return 'helloworld-webroot'}
-	get WWW_PORT() {return 8000}
-	
-} module.exports = ServerConfig
